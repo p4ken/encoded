69837ce5f6a529c03c46537d749cf50125e8188f
p4ken <commit@p4ken.skr.jp>
2022-11-27 08:01:45 +0900

    Encode to sjis

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,38 @@
 use std::{str::FromStr, vec::Vec};
 
+use litrs::StringLit;
 use proc_macro::{Literal, Span, TokenStream, TokenTree};
 
 #[proc_macro]
 pub fn shift_jis(tokens: TokenStream) -> TokenStream {
-    // dbg!(&tokens);
-
     let tokens = tokens.into_iter().collect::<Vec<_>>();
     let literal = match &tokens[..] {
         [TokenTree::Literal(x)] => x,
         [x] => return error(x.span(), "argument must be a string literal"),
-        _ => return error(Span::call_site(), "shift_jis! takes 1 argument2"),
+        [..] => return error(Span::call_site(), "shift_jis! takes 1 argument"),
     };
-    dbg!(&literal);
 
-    let literal = literal.to_string();
-    dbg!(&literal);
-    // 最初と最後が " かどうか？
-    // let utf8 = match literal.chars() {
-    //     [""] => panic!("yeaaah!"),
-    //     _ => literal,
-    // };
+    let utf8 = match StringLit::try_from(literal) {
+        Ok(x) => x,
+        Err(_) => return error(literal.span(), "argument must be a string literal"),
+    };
 
-    // let utf8 = input.to_string();
-    // let (sjis, _, fail) =encoding_rs::SHIFT_JIS.encode(&utf8);
-    // assert!(!fail);
-    // dbg!(sjis);
+    let (sjis, _, fail) = encoding_rs::SHIFT_JIS.encode(utf8.value());
+    if fail {
+        return error(
+            literal.span(),
+            "some characters couldn't convert to SHIFT_JIS",
+        );
+    }
 
-    [TokenTree::Literal(Literal::byte_string(&[
-        147u8, 250, 150, 123, 140, 234,
-    ]))]
-    .into_iter()
-    .collect::<TokenStream>()
+    [TokenTree::Literal(Literal::byte_string(&sjis[..]))]
+        .into_iter()
+        .collect::<TokenStream>()
 }
 
 fn error(span: Span, message: &str) -> TokenStream {
-    let tokens = TokenStream::from_str(&format!("compile_error!(\"{}\")", message)).unwrap();
-    tokens
+    TokenStream::from_str(&format!("compile_error!(\"{}\")", message))
+        .unwrap()
         .into_iter()
         .map(|mut t| {
             t.set_span(span);
