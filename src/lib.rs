f3131512e6fd15638a0866ca02192519990bed30
p4ken <commit@p4ken.skr.jp>
2022-11-26 18:50:01 +0900

    Always return sjis bytes of "日本語"

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,23 @@
-use proc_macro::TokenStream;
+use std::vec::Vec;
+
+use proc_macro::{TokenStream, TokenTree, Literal};
 
 #[proc_macro]
-pub fn shift_jis(input: TokenStream) -> TokenStream {
-    input
+pub fn shift_jis(tokens: TokenStream) -> TokenStream {
+    // tmp clone
+    let tokens_vec = tokens.clone().into_iter().collect::<Vec<_>>();
+    let literal = match tokens_vec.as_slice() {
+        [TokenTree::Literal(x)] => x,
+        [_] => panic!("Expected literal."), //return syn::Error::new(x.span().into(), "expected Literal").to_compile_error().into(),
+        _ => panic!("Expected single token."),
+    };
+    // literal.
+    dbg!(&literal);
+
+    // let utf8 = input.to_string();
+    // let (sjis, _, fail) =encoding_rs::SHIFT_JIS.encode(&utf8);
+    // assert!(!fail);
+    // dbg!(sjis);
+
+    [TokenTree::Literal(Literal::byte_string(&[147u8, 250, 150, 123, 140, 234]))].into_iter().collect::<TokenStream>()
 }
